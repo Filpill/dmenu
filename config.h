e3fbbe4e33f847af450f1700726ced7eedf78cf4
Filpill <filiplivancic@gmail.com>
2024-02-27 13:39:38 +0000

    added centering

--- config.h
+++ config.h
@@ -2,6 +2,8 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 950;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=11"
@@ -14,8 +16,8 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 5;
-static unsigned int columns    = 8;
+static unsigned int lines      = 8;
+static unsigned int columns    = 5;
 
 /*
  * Characters not considered part of a word while deleting words
