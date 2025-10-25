ee6c85bab1f3ebb574231136b93ae0ba06c37bfd
filpill <filiplivancic@gmail.com>
2025-10-25 20:51:29 +0100

    added nord style colors

--- config.h
+++ config.h
@@ -11,9 +11,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	   	     /*     fg         bg       */
-	[SchemeNorm] = { "#f0f0f0", "#011f03" },
-	[SchemeSel] = { "#eeeeee", "#038028" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#D8DEE9", "#2E3440" },
+	[SchemeSel] = { "#ECEFF4", "#3B4252" },
+	[SchemeOut] = { "#2E3440", "#88C0D0" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 10;
